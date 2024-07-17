16c853768f7559f8398072fb0ac18f644d3f0ef4
aaronkimbrooktec <218adk@gmail.com>
2024-07-17 01:11:50 -0400

    lebron james

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import '../styles/global.css';
+  import Nav from '$lib/components/Nav.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Nav />
+
+<main>
+  <slot />
+</main>
+
+<Footer />
